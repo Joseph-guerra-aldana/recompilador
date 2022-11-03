f8658d416b6562242a722a46539ad049cdac0a7e
byronrmrz <byronrmrz@gmail.com>
2022-11-02 21:48:12 -0600

    integracion whatsapp

--- lib/submit.dart
+++ lib/submit.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() => runApp(const SubmitButton());
+void _openWhatsapp() async {
+  //String phoneNumber = "55430213";
+  var url =
+      "https://wa.me/51956345?text=Estimado_señor_fulano_Mensaje_prueba_flutter";
+
+  await launchUrl(Uri.parse(url));
+}
 
 class SubmitButton extends StatelessWidget {
   const SubmitButton({super.key});
@@ -8,7 +16,9 @@ class SubmitButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return TextButton(
-      onPressed: null,
+      onPressed: () {
+        _openWhatsapp();
+      },
       style: ButtonStyle(
           // minimumSize: const Size(88, 36),
           backgroundColor: MaterialStateProperty.all(Colors.amber),
